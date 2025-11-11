aee0b5b2ee79ccb0264f1c09766a00dbda3dcec8
SnehGupt <31393580+SnehGupt@users.noreply.github.com>
2025-11-11 02:16:45 -0800

    Update banksmart_api.py

--- banksmart_api.py
+++ banksmart_api.py
@@ -45,4 +45,6 @@ def ticker_summary():
         return jsonify({"error": f"Failed to fetch data for {ticker}: {str(e)}"}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True)
+   import os
+port = int(os.environ.get("PORT", 5000))
+app.run(host="0.0.0.0", port=port)
